14a2a7b00c55c1759bd52bf9c03918a21dcfb51a
Kraig McFadden <kmcfadden95@gmail.com>
2024-09-14 16:33:06 -0400

    chore: fmt

--- inputs.tf
+++ inputs.tf
@@ -1,41 +1,41 @@
 variable "queue_name" {
-    type = string
-    nullable = false
-    description = "What you want this queue to be called"
+  type        = string
+  nullable    = false
+  description = "What you want this queue to be called"
 }
 
 variable "max_receive_count" {
-    type = number
-    description = "Number of times a message can go back on the queue before being DLQ'd"
-    default = 5
+  type        = number
+  description = "Number of times a message can go back on the queue before being DLQ'd"
+  default     = 5
 }
 
 variable "receive_wait_time_seconds" {
-    type = number
-    description = "How long a connection should linger if there are no messages. Max 20."
-    default = 20
+  type        = number
+  description = "How long a connection should linger if there are no messages. Max 20."
+  default     = 20
 }
 
 variable "visbility_timeout_seconds" {
-    type = number
-    description = "How long before a message is visible again on the queue if it's not deleted after receiving"
-    default = 30
+  type        = number
+  description = "How long before a message is visible again on the queue if it's not deleted after receiving"
+  default     = 30
 }
 
 variable "queue_message_retention_seconds" {
-    type = number
-    description = "How long a message can live on the queue before being automatically deleted"
-    default = 345600  // 4 days
+  type        = number
+  description = "How long a message can live on the queue before being automatically deleted"
+  default     = 345600 // 4 days
 }
 
 variable "dlq_message_retention_seconds" {
-    type = number
-    description = "How long a message can live on the dlq before being automatically deleted"
-    default = 1209600  // 14 days
+  type        = number
+  description = "How long a message can live on the dlq before being automatically deleted"
+  default     = 1209600 // 14 days
 }
 
 variable "encrypt_at_rest" {
-    type = bool
-    description = "Whether messages should be encrypted on the queue"
-    default = true
+  type        = bool
+  description = "Whether messages should be encrypted on the queue"
+  default     = true
 }
--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_sqs_queue" "queue" {
-  name = var.queue_name
-  receive_wait_time_seconds = var.receive_wait_time_seconds
+  name                       = var.queue_name
+  receive_wait_time_seconds  = var.receive_wait_time_seconds
   visibility_timeout_seconds = var.visbility_timeout_seconds
-  message_retention_seconds = var.queue_message_retention_seconds
-  sqs_managed_sse_enabled = var.encrypt_at_rest
+  message_retention_seconds  = var.queue_message_retention_seconds
+  sqs_managed_sse_enabled    = var.encrypt_at_rest
 
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.dlq.arn
@@ -12,11 +12,11 @@ resource "aws_sqs_queue" "queue" {
 }
 
 resource "aws_sqs_queue" "dlq" {
-  name = "${var.queue_name}-dlq"
-  receive_wait_time_seconds = var.receive_wait_time_seconds
+  name                       = "${var.queue_name}-dlq"
+  receive_wait_time_seconds  = var.receive_wait_time_seconds
   visibility_timeout_seconds = var.visbility_timeout_seconds
-  message_retention_seconds = var.dlq_message_retention_seconds
-  sqs_managed_sse_enabled = var.encrypt_at_rest
+  message_retention_seconds  = var.dlq_message_retention_seconds
+  sqs_managed_sse_enabled    = var.encrypt_at_rest
 }
 
 resource "aws_sqs_queue_redrive_allow_policy" "dlq_redrive_allow_policy" {
